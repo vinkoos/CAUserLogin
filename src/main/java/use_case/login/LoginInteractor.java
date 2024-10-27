47da9a88c3835316b070ed5bd58261216dfe02c2
yitong an <ethan.an2004@gmail.com>
2024-10-27 03:45:37 -0400

    change nothing

--- src/main/java/use_case/login/LoginInteractor.java
+++ src/main/java/use_case/login/LoginInteractor.java
@@ -19,7 +19,7 @@ public class LoginInteractor implements LoginInputBoundary {
     public void execute(LoginInputData loginInputData) {
         final String username = loginInputData.getUsername();
         final String password = loginInputData.getPassword();
-        this.userDataAccessObject.setCurrentUser("paul");
+        this.userDataAccessObject.setCurrentUser("Paul");
         if (!userDataAccessObject.existsByName(username)) {
             loginPresenter.prepareFailView(username + ": Account does not exist.");
         }
