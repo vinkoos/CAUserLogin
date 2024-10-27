1b976b26032f3652c5c396323116494f1c7faf79
yitong an <ethan.an2004@gmail.com>
2024-10-27 02:24:43 -0400

    change Logintest

--- src/test/java/use_case/login/LoginInteractorTest.java
+++ src/test/java/use_case/login/LoginInteractorTest.java
@@ -23,12 +23,13 @@ public class LoginInteractorTest {
         UserFactory factory = new CommonUserFactory();
         User user = factory.create("Paul", "password");
         userRepository.save(user);
+        String return_name = userRepository.getCurrentUser(user);
 
         // This creates a successPresenter that tests whether the test case is as we expect.
         LoginOutputBoundary successPresenter = new LoginOutputBoundary() {
             @Override
             public void prepareSuccessView(LoginOutputData user) {
-                assertEquals("Paul", user.getUsername());
+                assertEquals("Paul", return_name);
             }
 
             @Override
